0e85a12a80b27a867de7a2f47742421e11bf43c8
Micah Banschick <micha.ban@gmail.com>
2021-03-23 19:46:15 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
+// import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let review = {text: this.state.text, restaurantId: this.props.restaurantId}
+    this.props.addReview(review)
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
       <div>
