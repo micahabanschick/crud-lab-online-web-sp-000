ea1892be5916b86e87940d4a11edabc4c9a7e6a4
Micah Banschick <micha.ban@gmail.com>
2021-03-23 19:44:18 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  
+  constructor() {
+    super()
+    this.state = { text: '' }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
       <div>
