077bbed4b2101e54d7f7cf6edb009af79921684c
Micah Banschick <micha.ban@gmail.com>
2021-03-23 19:49:27 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -8,8 +8,8 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput addReview={this.props.addReview} restaurantId={this.props.restaurantId} />
+        <Reviews reviews={this.props.reviews} deleteReview={this.props.deleteReview} />
       </div>
     )
   }
